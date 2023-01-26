d56ed099b96ea6367658f6cc1e36ce78ed30d199
RTids <121754534+RTids@users.noreply.github.com>
2023-01-26 23:51:04 +0000

    JS can render products based on category selected

--- product-data.js
+++ product-data.js
@@ -1,38 +1,26 @@
 export const products = [
     {
-        name: 'Heating Coil 1',
+        name: 'Conduction Coil',
         picture: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1839&q=80',
         link: 'www.google.com',
         category: 'coil',
     },
     {
-        name: 'Heating Coil 3',
+        name: 'Heating Element',
         picture: 'https://plus.unsplash.com/premium_photo-1664439520331-cad0f644f76b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
-        link: 'www.ebay.co.uk'
+        link: 'www.ebay.co.uk',
+        category: 'heater'
     },
     {
-        name: 'Heating Coil 3',
-        picture: 'https://plus.unsplash.com/premium_photo-1664439520331-cad0f644f76b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
-        link: 'www.ebay.co.uk'
+        name: 'Heating Element 2',
+        picture: 'https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        link: 'www.ebay.co.uk',
+        category: 'heater'
     },
     {
-        name: 'Heating Coil 3',
-        picture: 'https://plus.unsplash.com/premium_photo-1664439520331-cad0f644f76b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
-        link: 'www.ebay.co.uk'
-    },
-    {
-        name: 'Heating Coil 3',
-        picture: 'https://plus.unsplash.com/premium_photo-1664439520331-cad0f644f76b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
-        link: 'www.ebay.co.uk'
-    },
-    {
-        name: 'Heating Coil 3',
-        picture: 'https://plus.unsplash.com/premium_photo-1664439520331-cad0f644f76b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
-        link: 'www.ebay.co.uk'
-    },
-    {
-        name: 'Heating Coil 3',
-        picture: 'https://plus.unsplash.com/premium_photo-1664439520331-cad0f644f76b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
-        link: 'www.ebay.co.uk'
+        name: 'Conduction Coil 2',
+        picture: 'https://images.unsplash.com/photo-1433162653888-a571db5ccccf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        link: 'www.google.com',
+        category: 'coil',
     },
 ]
--- products.js
+++ products.js
@@ -1,13 +1,47 @@
 import {products} from './product-data.js'
-const renderBtn = document.getElementById('render')
+
+const radiosEl = document.getElementById('radios')
 
 
+//Listens for what Cateogory button is selected
+radiosEl.addEventListener('change', pullSelectedCategory)
+
+//Pull through products when specific category is selected
+function pullSelectedCategory(){
+    const selected = document.querySelector("input[type='radio']:checked").value
+    const selectedCategoryArray = products.filter(function(product){
+        if (product.category === selected){
+            return product.category.includes(selected)
+        }
+    })
+    
+    //Produce HTML to render
+    let productHTML = ''
+
+    selectedCategoryArray.forEach(function(product) {
+    productHTML += `
+        <div class='product-tile ${product.category}'>
+            <img src='${product.picture}' alt='image of product'>
+            <p>${product.name}<p>
+            <p>More Information <a href='${product.link}'>Here</a><p>
+        </div>
+    `
+})
+
+//Render HTML to page
+document.getElementById('product-list').innerHTML = productHTML
+}
+
+
+
+
+//These load all products on page initially
 function getProductHTML(){
     let productHTML = ''
 
     products.forEach(function(product) {
     productHTML += `
-        <div>
+        <div class='product-tile ${product.category}'>
             <img src='${product.picture}' alt='image of product'>
             <p>${product.name}<p>
             <p>More Information <a href='${product.link}'>Here</a><p>
@@ -17,12 +51,9 @@ function getProductHTML(){
 return productHTML
 }
 
+
 function renderProducts() {
     document.getElementById('product-list').innerHTML = getProductHTML()
 }
 
-
-    renderProducts()
-
-
-//pull through products when specific category is selected
+renderProducts()
