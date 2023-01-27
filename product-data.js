e65083ebe9faf6d7c32edebc02a2b0f4b3dd15b7
RTids <121754534+RTids@users.noreply.github.com>
2023-01-27 10:58:50 +0000

    Added Product Categories | Start of Contact page

--- product-data.js
+++ product-data.js
@@ -1,26 +1,38 @@
 export const products = [
     {
-        name: 'Conduction Coil',
+        name: 'Temperature Controller 1',
         picture: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1839&q=80',
         link: 'www.google.com',
-        category: 'coil',
+        category: 'temperatureController',
     },
     {
-        name: 'Heating Element',
+        name: 'Temperature Sensor 1',
         picture: 'https://plus.unsplash.com/premium_photo-1664439520331-cad0f644f76b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
         link: 'www.ebay.co.uk',
-        category: 'heater'
+        category: 'temperatureSensor'
     },
     {
-        name: 'Heating Element 2',
+        name: 'Temperature Sensor 2',
         picture: 'https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
         link: 'www.ebay.co.uk',
-        category: 'heater'
+        category: 'temperatureSensor'
     },
     {
-        name: 'Conduction Coil 2',
+        name: 'Temperature Controller 2',
         picture: 'https://images.unsplash.com/photo-1433162653888-a571db5ccccf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
         link: 'www.google.com',
-        category: 'coil',
+        category: 'temperatureController',
+    },
+    {
+    name: 'Towel Rail - Industrial',
+    picture: 'https://images.unsplash.com/photo-1496450080853-5f78c43af9e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    link: 'www.google.com',
+    category: 'towelRail',
+    },
+    {
+        name: 'Silicone Rubber 1',
+        picture: 'https://images.unsplash.com/photo-1496450080853-5f78c43af9e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        link: 'www.google.com',
+        category: 'siliconeRubber',
     },
 ]
--- products.js
+++ products.js
@@ -2,12 +2,12 @@ import {products} from './product-data.js'
 
 const radiosEl = document.getElementById('radios')
 
-
 //Listens for what Cateogory button is selected
 radiosEl.addEventListener('change', pullSelectedCategory)
 
 //Pull through products when specific category is selected
 function pullSelectedCategory(){
+    let productHTML = ''
     const selected = document.querySelector("input[type='radio']:checked").value
     const selectedCategoryArray = products.filter(function(product){
         if (product.category === selected){
@@ -16,10 +16,12 @@ function pullSelectedCategory(){
     })
     
     //Produce HTML to render
-    let productHTML = ''
+    
 
     selectedCategoryArray.forEach(function(product) {
-    productHTML += `
+
+    
+        productHTML += `
         <div class='product-tile ${product.category}'>
             <img src='${product.picture}' alt='image of product'>
             <p>${product.name}<p>
